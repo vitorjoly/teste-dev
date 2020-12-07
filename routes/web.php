fcf9ee0a55512aa5f5ffc392810f229b4d73bde8
vitorjoly <vitormunhoz@live.com>
2020-12-07 01:01:53 -0300

    status_4

--- routes/web.php
+++ routes/web.php
@@ -18,15 +18,15 @@ Auth::routes();
 
 Route::get('/', function () {
     return view('welcome');
-});
+})->middleware('auth');
 
-Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
+Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home')->middleware('auth');
 
 //Rota da visualizacao do historico
-Route::get('/historico', [App\Http\Controllers\CapturaController::class, 'historico'])->name('historico');
+Route::get('/historico', [App\Http\Controllers\CapturaController::class, 'historico'])->name('historico')->middleware('auth');
 
 //Rota de captura de informacoes
-Route::post('/captura', [App\Http\Controllers\CapturaController::class, 'captura'])->name('captura');
+Route::post('/captura', [App\Http\Controllers\CapturaController::class, 'captura'])->name('captura')->middleware('auth');
 
 //Rota para deletar os artigos
-Route::get('captura/deletar/{artigo}', [App\Http\Controllers\CapturaController::class, 'deletar'])->name('deletar');
+Route::get('captura/deletar/{artigo}', [App\Http\Controllers\CapturaController::class, 'deletar'])->name('deletar')->middleware('auth');
